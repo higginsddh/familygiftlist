37649880d4a2c590d7e07430c85874cd2d762f9e
Daniel Higgins <daniel.higgins@gmail.com>
2023-01-09 08:29:14 -0500

    start of file upload

--- src/components/BaseForm.tsx
+++ src/components/BaseForm.tsx
@@ -1,5 +1,6 @@
 import {
   Button,
+  FileInput,
   Group,
   LoadingOverlay,
   Modal,
@@ -7,6 +8,7 @@ import {
   TextInput,
 } from "@mantine/core";
 import { useForm } from "@mantine/form";
+import { useState } from "react";
 
 type FormData = {
   name: string;
@@ -28,15 +30,33 @@ export function BaseForm({
   const form = useForm<FormData>({
     initialValues: defaultFormData,
   });
+  const [file, setFile] = useState<File | null>(null);
+  const [fileUploading, setFileUploading] = useState(false);
 
   return (
     <>
       <Modal opened={true} onClose={() => onClose()} title="Add Item">
-        {saving ? <LoadingOverlay visible overlayBlur={2} /> : null}
+        {saving || fileUploading ? (
+          <LoadingOverlay visible overlayBlur={2} />
+        ) : null}
 
         <form
           onSubmit={form.onSubmit((values) => {
-            onSave(values);
+            if (file) {
+              const fileData = new FormData();
+              fileData.append("file", file);
+              fileData.append("upload_preset", "dwi7jlm4");
+              fileData.append("cloud_name", "dutizqtbe");
+              fetch("https://api.cloudinary.com/v1_1/dutizqtbe/image/upload", {
+                method: "post",
+                body: fileData,
+              })
+                .then((resp) => resp.json())
+                .then((data) => console.log(data))
+                .catch((err) => console.error(err));
+            } else {
+              onSave(values);
+            }
           })}
         >
           <TextInput
@@ -55,6 +75,13 @@ export function BaseForm({
           />
 
           <Textarea label="Notes" mb="md" {...form.getInputProps("notes")} />
+
+          <FileInput
+            placeholder="Pick photo"
+            label="Image of item"
+            accept="image/*"
+            onChange={(f) => setFile(f)}
+          />
 
           <Group mt="md" position="right">
             <Button type="submit">Save</Button>
