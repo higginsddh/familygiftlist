977d0289b30191d5d0e7da304d19bf8e46dfa5e7
Daniel Higgins <daniel.higgins@gmail.com>
2023-01-12 08:33:12 -0500

    show img

--- src/components/FamilyMemberItem.tsx
+++ src/components/FamilyMemberItem.tsx
@@ -11,7 +11,23 @@ import { useState } from "react";
 import { trpc } from "../utils/trpc";
 import { EditItem } from "./EditItem";
 
+import { Cloudinary } from "@cloudinary/url-gen";
+import { AdvancedImage } from "@cloudinary/react";
+import { thumbnail, scale } from "@cloudinary/url-gen/actions/resize";
+
 export function FamilyMemberItem({ giftItem }: { giftItem: GiftItem }) {
+  // Create and configure your Cloudinary instance.
+  const cld = new Cloudinary({
+    cloud: {
+      cloudName: "dutizqtbe",
+    },
+  });
+
+  // Use the image with public ID, 'front_face'.
+  const myImage = cld.image("public_uploads/txxu26bupsdzisdmxpmw");
+
+  myImage.resize(thumbnail().width(150)).format("png");
+
   const [showDeleteConfirm, setShowDeleteConfirm] = useState(false);
   const [showEdit, setShowEdit] = useState(false);
   const utils = trpc.useContext();
@@ -33,6 +49,10 @@ export function FamilyMemberItem({ giftItem }: { giftItem: GiftItem }) {
         <Text size="sm" color="dimmed">
           {giftItem.notes}
         </Text>
+
+        <Card.Section mt="sm" ml="xs">
+          <AdvancedImage cldImg={myImage} />
+        </Card.Section>
 
         <div>
           <Button
